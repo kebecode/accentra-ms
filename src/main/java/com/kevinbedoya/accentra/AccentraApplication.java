c137d4f97f86f43ffeb21aea2b08f724d2fc5e99
Kevin Bedoya <1355330+kebecode@users.noreply.github.com>
2025-05-24 20:35:47 -0500

    Initial commit

---
+++ src/main/java/com/kevinbedoya/accentra/AccentraApplication.java
@@ -0,0 +1,13 @@
+package com.kevinbedoya.accentra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccentraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kevinbedoya/accentra/AccentraMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinbedoya.accentra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentraMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
